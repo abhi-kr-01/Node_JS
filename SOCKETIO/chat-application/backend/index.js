b79b0a1d775ce2cc2dcc633c0aaf9b02097af407
abhi-kr-01 <abhikrsr7890@gmail.com>
2026-05-27 00:12:45 +0530

    made simple chat-application with exiting features

--- SOCKETIO/chat-application/backend/index.js
+++ SOCKETIO/chat-application/backend/index.js
@@ -60,18 +60,76 @@ const io = new Server(server, {
     }
 })
 
+let onlineUser = 0;
+
 //listen for connection 
 // io.on("connection",(socket) => {
 //     console.log("client connected");
 //     console.log(socket.id)
 // })
+// io.on("connection", (socket) => {
+
+//     console.log("Connected:", socket.id);
+
+//     socket.on("disconnect", () => {
+//         console.log("Disconnected:", socket.id);
+//     });
+
+// });
+
+// custom event handling for client and server 
+
+// io.on("connection", (socket) => {
+
+//     console.log("Connected:", socket.id);
+
+//     //socket.on(evenetName,callback)  -- listen the event
+//     socket.on('message',(data)=> {
+//         console.log("recieved:",data);
+//         socket.emit("reply","hello user from server");
+//     })
+
+//     socket.on("disconnect", () => {
+//         console.log("Disconnected:", socket.id);
+//     });
+
+// });
+
+
+
+// now create a socket connection in which one client send message it reflect to all client which is connected
 io.on("connection", (socket) => {
 
     console.log("Connected:", socket.id);
+    socket.on('join-chat',(username)=>{
+        onlineUser++;
+        socket.username = username;   // add custom method "username" on socket like id -- yes we can add custom to disconnect b/c disconnect not pass any username to tell every one xyz user diconnected
+        
+        console.log(onlineUser);  // testing
 
-    socket.on("disconnect", () => {
-        console.log("Disconnected:", socket.id);
-    });
+        socket.broadcast.emit("user-joined", `${username} joined chat`)   // broadcast -- to show everyone except user
+
+        io.emit("online-users",onlineUser);
+    })
+
+    socket.on('send-message',(data) => {
+        io.emit("recieved-message",{
+            text: data.text,
+            senderId: socket.id,
+            username: data.username
+        });
+    })
+
+    socket.on('disconnect',()=>{
+        console.log("Disconnected:",socket.id);
+        onlineUser--;
+
+        socket.broadcast.emit('user-left',`${socket.username} left chat`);
+
+        io.emit("online-users",onlineUser);
+
+        
+    })
 
 });
 
--- SOCKETIO/chat-application/frontend/frontend-chat-application/src/App.jsx
+++ SOCKETIO/chat-application/frontend/frontend-chat-application/src/App.jsx
@@ -1,53 +1,244 @@
-// import { useState } from 'react'
-// import { useEffect } from 'react'
-// import axios from 'axios'
-
-// function App() {
-  
-//   const [message,setMessage] = useState("");
-
-//   useEffect(()=>{
-//     const getData = async () => {
-//       try {
-//         const response = await axios.get("http://localhost:3000/api/message")
-//         setMessage(response.data.message);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-
-//     getData()
-//   },[])
-
-//   return(
-//     <div>
-//       <h1> {message} </h1>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useEffect, useRef } from "react";
 import { socket } from "./socket";
+import { useState } from "react";
+import "./App.css"
 
 function App() {
 
+    const inputRef = useRef(null);
+    const usernameRef = useRef("");
+
+    const [message,setMessage] = useState("");
+    const [messages,setMessages] = useState([]);
+    const [mySocketId,setMySocketId] = useState("");
+    const [username,setUsername] = useState("");
+    const [onlineUsers,setOnlineUsers] = useState(0);
+
+    const handleKeyDown = (e) => {
+        if(e.key === "Enter"){
+            sendMessage();
+        }
+    }
+
+    useEffect(() => {
+        const name = prompt("Enter username:");
+
+        const finalName = name?.trim() || "Ananonymous";
+        setUsername(finalName);
+
+        usernameRef.current = finalName;
+    },[])
+
     useEffect(() => {
 
-        socket.connect()
+        // socket.on(
+        //     "connect",
+        //     () => {
 
+        //         // console.log(
+        //         //     "client connected",socket.id
+        //         // );
+        //         // console.log("My Socket ID:",socket.id);
+
+        //         // socket.emit(
+        //         //     "message",
+        //         //     "Hello server"
+        //         // );
+
+
+        //     }
+        // );
+
+        // socket.on(
+        //     "reply",
+        //     (data) => {
+
+        //         console.log(
+        //             "server says:",
+        //             data
+        //         );
+        //     }
+        // );
+
+        //socket.off("receive-message");
+        socket.on('recieved-message',(message)=>{
+            setMessages((prev) => [...prev,message])
+        })
+
+        
+        socket.on('connect',()=>{
+            socket.emit('join-chat',usernameRef.current);
+        })
+
+        socket.on('user-joined',(message) => {
+            setMessages((prev) => [
+                ...prev,
+                {
+                    system: true,   // to style system messages differently.
+                    text: message
+                }
+            ])
+        });
+
+        socket.on("user-left",(message) => {
+            setMessages((prev) => [
+                ...prev,
+                {
+                    system: true,   // to style system messages differently.
+                    text: message
+                }
+            ])
+        });
+
+        socket.on("online-users",(count) => {
+            setOnlineUsers(count);
+        })
+
+        // connect AFTER listeners
+        socket.connect();
+
+        inputRef.current?.focus()
 
         return () => {
+
+            socket.off("connect");
+            //socket.off("reply");
+
+            socket.off(
+                "received-message"
+            );
+
+            socket.off(
+                "online-users"
+            );
+
+            socket.off(
+                "user-joined"
+            );
+
+            socket.off(
+                "user-left"
+            );
+
             socket.disconnect();
-        };
+            };
 
     }, []);
 
+    const sendMessage = () => {
+        if(!message.trim){
+            return ;
+        }
+
+        socket.emit('send-message',{text:message,username});
+        setMessage("");
+        inputRef.current?.focus()
+    }
+
+
     return (
-        <h1>Socket Connected</h1>
+
+        <div className="chat-container">
+            <h1>
+                Mini Chat
+            </h1>
+            <h3>
+                Online Users: {onlineUsers}
+            </h3>
+
+            <div className="chat-box">
+
+                {
+messages.map(
+(msg,index)=>{
+
+if(msg.system){
+
+return(
+
+<div
+key={index}
+className=
+"system-message"
+>
+{msg.text}
+</div>
+
+);
+}
+
+const isMine =
+msg.senderId ===
+socket.id;
+
+return(
+
+<div
+key={index}
+className={
+isMine
+?
+"message-right"
+:
+"message-left"
+}
+>
+
+<div
+className=
+"message-user"
+>
+{msg.username}
+</div>
+
+<div
+className=
+"message-text"
+>
+{msg.text}
+</div>
+
+</div>
+
+);
+
+})
+}
+
+            </div>
+
+            <div
+              className=
+              "input-area"
+            >
+
+                <input
+                    type="text"
+                    ref={inputRef}
+                    onKeyDown={ handleKeyDown }
+                    value={message}
+                    onChange={
+                        (e) =>
+                        setMessage(
+                            e.target
+                            .value
+                        )
+                    }
+                    placeholder=
+                    "Type message ..."
+                />
+
+                <button
+                  onClick={
+                    sendMessage
+                  }
+                >
+                    send
+                </button>
+
+            </div>
+
+        </div>
     );
 }
 
